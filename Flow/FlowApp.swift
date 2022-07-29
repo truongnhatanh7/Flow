e3123c60ed663cd60e919820caf4b829e7e98fe3
truongnhatanh7 <truongnhatanhw@gmail.com>
2022-07-29 10:35:48 +0700

    Initial Commit

---
+++ Flow/FlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlowApp.swift
+//  Flow
+//
+//  Created by Truong Nhat Anh on 29/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct FlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
